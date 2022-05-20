09ae8163f18a5596902a639c4a230315b3945b52
Maria-Anastasia Moustaka <mariamou7@windowslive.com>
2022-05-20 12:30:48 +0300

    Update forest_code.ino

--- Code/forest_code.ino
+++ Code/forest_code.ino
@@ -4,57 +4,62 @@
 */
 
 //Initializations
-int buzzer = 4;
-int smokePin = A0;
-int tempPin = A1;
-int voltPin = A2;
+//constants won't change
+int buzzer = 4; //initialize digital pin 4 as "buzzer"
+int smokePin = A0; //initialize analog pin 0 as "smokePin"
+int tempPin = A1; //initialize analog pin 1 as "tempPin"
+int voltPin = A2; //initialize analog pin 2 as "voltPin"
 
 //The setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(buzzer, OUTPUT);
-  pinMode(smokePin, INPUT);
-  pinMode(tempPin, INPUT);
-  pinMode(voltPin, INPUT);
+  pinMode(buzzer, OUTPUT); //initialize "buzzer" as an output
+  pinMode(smokePin, INPUT); //initialize "smokePin" as an input
+  pinMode(tempPin, INPUT); //initialize "tempPin" as an input
+  pinMode(voltPin, INPUT); //initialize "voltPin" as an input
   
+  //We'll use the serial monitor to view sensors' output
   Serial.begin(9600);
 }
 
 //The loop function runs over and over again forever
 void loop() {
-  int tempValue = analogRead(tempPin); 
-  float voltage = tempValue*(5.0/1024.0);
-  float celsius = voltage*50;
+  int tempValue = analogRead(tempPin); //read the value from temperature sensor
+  float voltage = tempValue*(5.0/1024.0); //convert the value to voltage
+  float celsius = voltage*50; //convert the voltage to celsius
   
+  //Print the temperature
   Serial.print("Temperature: ");
   Serial.print(celsius);
   Serial.print(" *C");
   Serial.println();
   
-  delay(1000);
+  delay(1000); //wait 1 sec
 
-  int smoke = analogRead(smokePin);
+  int smoke = analogRead(smokePin); //read the value from smoke sensor
 
+  //Print the smoke
   Serial.print("Smoke: ");
   Serial.println(smoke);
   
   // Checks if it has reached the threshold value
-  if (smoke > 540 && celsius > 200) //540, 60
+  if (smoke > 540 && celsius > 200)
   {
-    tone(buzzer, 255);
-    delay(1000);
-    noTone(buzzer);
-    delay(100);
+    tone(buzzer, 255); //send 0.225KHz sound signal
+    delay(1000); //for 1 sec
+    noTone(buzzer); //stop sound
+    delay(100); //for 0.1 sec
 
-    tone(buzzer, 255);
-    delay(1000);
-    noTone(buzzer);
-    delay(100);
+    tone(buzzer, 255); //send 0.225KHz sound signal
+    delay(1000); //for 1 sec
+    noTone(buzzer); //stop sound
+    delay(100); //for 0.1 sec
 
-    tone(buzzer, 255);
-    delay(1000);
-    noTone(buzzer);
-    delay(100);
+    tone(buzzer, 255); //send 0.225KHz sound signal
+    delay(1000); //for 1 sec
+    noTone(buzzer); //stop sound
+    delay(100); //for 0.1 sec
     
+    ////Print Alarm
     Serial.println("Alarm!!!");
   }
   else
@@ -62,13 +67,18 @@ void loop() {
     noTone(buzzer);
   }
   
-  delay(5000);
+  delay(5000); //wait 5 sec
   
+  //Calculating the battery charging voltage
   int volts = analogRead(voltPin);
+  
+  //Print the volts
   Serial.print("Volts: ");
   Serial.println(volts);
   
-  float v12 = (volts*12.0)/1023;
+  float v12 = (volts*12.0)/1023; //convert the value of the analog measurement (0-1023) to volts (0-12)
+  
+  //Print the volts
   Serial.print("V12: ");
   Serial.println(v12);
   Serial.println("__________________________________________");
